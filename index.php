11a22636a7461bef54e24b5579169853fe6b832f
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-15 16:02:41 +0200

    Add the Structure: Add the Html Structure and the Array of the Hotels. Print the Array in the Html Page

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4,
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2,
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1,
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5,
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50,
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+</head>
+<body>
+    <h1>Hotels List:</h1>
+
+    <?php foreach ($hotels as $hotel) : ?>
+
+        <p>
+            Hotel Name: <?= $hotel['name'] ?>
+            <br>
+            
+            Descripiton: <?= $hotel['description'] ?>
+            <br>
+
+            Parking: <?= $hotel['parking'] ?>
+            <br>
+
+            Vote: <?= $hotel['vote'] ?>
+            <br>
+
+            Distance to Center: <?= $hotel['distance_to_center'] ?>
+            <br>
+        </p>
+
+    <?php endforeach; ?>
+
+
+</body>
+</html>
